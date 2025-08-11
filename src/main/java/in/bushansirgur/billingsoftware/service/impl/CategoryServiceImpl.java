d3291f45d56356d1a2419baf6cc2d545c184d4bd
Anton Almishev <antonalmishev@abv.bg>
2025-08-11 10:18:51 +0300

    start pos for shops backend

---
+++ src/main/java/in/bushansirgur/billingsoftware/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,74 @@
+package in.bushansirgur.billingsoftware.service.impl;
+
+import in.bushansirgur.billingsoftware.entity.CategoryEntity;
+import in.bushansirgur.billingsoftware.io.CategoryRequest;
+import in.bushansirgur.billingsoftware.io.CategoryResponse;
+import in.bushansirgur.billingsoftware.repository.CategoryRepository;
+import in.bushansirgur.billingsoftware.repository.ItemRepository;
+import in.bushansirgur.billingsoftware.service.CategoryService;
+import in.bushansirgur.billingsoftware.service.FileUploadService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final ItemRepository itemRepository;
+    private final FileUploadService fileUploadService;
+
+    public CategoryResponse add(CategoryRequest request, MultipartFile file) throws IOException {
+        String imgUrl = fileUploadService.uploadFile(file);
+        CategoryEntity newCategory = convertToEntity(request);
+        newCategory.setImgUrl(imgUrl);
+        newCategory = categoryRepository.save(newCategory);
+        return convertToResponse(newCategory);
+    }
+
+    @Override
+    public List<CategoryResponse> read() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(categoryEntity -> convertToResponse(categoryEntity))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void delete(String categoryId) {
+        CategoryEntity existingCategory = categoryRepository.findByCategoryId(categoryId)
+                .orElseThrow(() -> new RuntimeException("Category not found: "+categoryId));
+        fileUploadService.deleteFile(existingCategory.getImgUrl());
+        categoryRepository.delete(existingCategory);
+    }
+
+    private CategoryResponse convertToResponse(CategoryEntity newCategory) {
+        Integer itemsCount = itemRepository.countByCategoryId(newCategory.getId());
+        return CategoryResponse.builder()
+                .categoryId(newCategory.getCategoryId())
+                .name(newCategory.getName())
+                .description(newCategory.getDescription())
+                .bgColor(newCategory.getBgColor())
+                .imgUrl(newCategory.getImgUrl())
+                .createdAt(newCategory.getCreatedAt())
+                .updatedAt(newCategory.getUpdatedAt())
+                .items(itemsCount)
+                .build();
+    }
+
+    private CategoryEntity convertToEntity(CategoryRequest request) {
+        return CategoryEntity.builder()
+                .categoryId(UUID.randomUUID().toString())
+                .name(request.getName())
+                .description(request.getDescription())
+                .bgColor(request.getBgColor())
+                .build();
+    }
+}
